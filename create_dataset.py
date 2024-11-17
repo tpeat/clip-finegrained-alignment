1db2892d7b79b5655c031a59edc924fa0237ecfb
Karthik Kundurthy <kkundurthy3@login-ice-2.pace.gatech.edu>
2024-11-17 12:49:25 -0500

    create_dataset: Randomly remove backgrounds using rembg library
    Signed-off-by: Karthik Kundurthy <kkundurthy3@login-ice-2.pace.gatech.edu>

--- create_dataset.py
+++ create_dataset.py
@@ -7,8 +7,8 @@ import logging
 import argparse
 from PIL import Image
 import random
-
-
+import rembg
+import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,9 +17,10 @@ logger = logging.getLogger(__name__)
 1. Set output directory
 2. Iterate through each coco image in test and validation
 3. Randomly sample an image net class and randomly sample an object within that class
-4. Randomly Resize the image to something between 16 x 16 and 32 x 32
-5. Randomly overlap the resized ImageNet object over the coco object
-6. Save the image to the output directory and create the CountBench Json format, with image attribute set to where the image was saved
+4. Remove the background using rembg: https://medium.com/@HeCanThink/rembg-effortlessly-remove-backgrounds-in-python-c2248501f992
+5. Randomly Resize the image to something between 16 x 16 and 32 x 32
+6. Randomly overlap the resized ImageNet object over the coco object
+7. Save the image to the output directory and create the CountBench Json format, with image attribute set to where the image was saved
 '''
 
 validation_dir = "dataset/3/FastAI_ImageNet_v2/val"
@@ -54,17 +55,34 @@ def main(args):
         coco = COCO("dataset/coco/annotations/instances_val2017.json")
         pass
     elif (args.function=="data"):
+        coco = COCO("dataset/coco/annotations/captions_train2017.json")
+        logger.info("CREATING TRAIN DATASET")
         for file in os.listdir(img_save_dir):
+            logger.info(f"FILE NAME: {file}")
             class_sample = random.sample(img_net_classes, 1)
             small_obj_class = class_sample[0]
             candidate_imgs= [f for f in os.listdir(f"{validation_dir}/{small_obj_class}")]
             count = random.randint(1,10)
             selected_imgs = random.sample(candidate_imgs,count)
+            
+            try:
+                orig_img = Image.open(os.path.join(img_save_dir,file)).convert("RGBA")
+            except Exception:
+                continue
 
-            orig_img = Image.open(os.path.join(img_save_dir,file)).convert("RGBA")
-
+            rem_back = False
             for img in selected_imgs:
                 small_img = Image.open(os.path.join(validation_dir,small_obj_class,img)).convert("RGBA")
+
+                # Applying rembg for removing background, pulled from medium code
+
+                if rem_back:
+                    rem_back = True
+                    logger.info("REMOVING BACKGROUND")
+                    input_array = np.array(small_img)
+                    output_array = rembg.remove(input_array)
+                    small_img = Image.fromarray(output_array)
+
                 dim = random.randint(16,32)
 
                 small_img = small_img.resize((dim,dim))
@@ -75,20 +93,35 @@ def main(args):
                 rand_y_pos = random.randint(0,orig_img.height - small_img.height)
                 orig_img.paste(small_img,(rand_x_pos,rand_y_pos))
 
+            rem_back = False
 
-            save_path = os.path.join("dataset/final_images",f"{small_obj_class}_{count}_{file}")
-            orig_img.convert("RGB").save(save_path)
+            try:
+                logger.info("SAVING IMG ...")
+                save_path = os.path.join("dataset/final_images",f"{small_obj_class}_{count}_{file}")
+                orig_img.convert("RGB").save(save_path)
+            except Exception as e:
+                logger.info(f"ERROR OCCURED WHILE SAVING IMG: {e}")
             
+            # getting captions
+            caption = coco.loadAnns(coco.getAnnIds(imgIds=file))
+            # caption = "".join([c['caption'] for c in caption_res])
+
+
             bench_ele = {
                 "Question": f"How many {small_obj_class}(s) are in this image?", 
                 "Class Label": f"{small_obj_class}", 
                 "Count": f"{count}",
-                "Image Path": f"{save_path}"
+                "Image Path": f"{save_path}",
+                "Original Caption": f"{caption}",
+                "Resolution": f"{dim} x {dim}",
+                "Background": f"{rem_back}"
             }
 
+            logger.info("ADDING ELE ...")
             # at inference timem we are just iterating through this and getting the corresponding img path
             ele_path = os.path.join(count_to_small_path,f"{small_obj_class}_{count}_{file}".removesuffix(".jpg"))
             ele_path = f"{ele_path}.json"
+            logger.info(f"SAVING TO {ele_path}!")
             try:
                 with open(ele_path,"x") as file:
                     json.dump(bench_ele, file, indent=4)
